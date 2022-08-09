f77eab46191bc9803d0e314172481ba0bb30b6c8
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-08-08 18:23:48 -0600

    change program message and add vehicle class

---
+++ GitBranchingPracticeTwo/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitBranchingPracticeTwo
+{
+    public abstract class Vehicle
+    {
+        public string Name { get; set; }
+
+        public virtual void Go()
+        {
+            Console.WriteLine("Vehicle is going");
+        }
+    }
+}
